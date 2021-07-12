daf3e3ff4a68d6b3c2c72877d854c1347c387cf7
bahram-ghahari <bahram.ghahari@gmail.com>
2021-07-12 17:22:58 -0400

    initial commit

---
+++ Exe/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using filemon.Monitor;
+
+namespace filemon.Exe
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Watcher watcher = new Watcher();
+            watcher.Path = args.Length>0 ? args[0] : "/home/cleantie/Documents/git/filemon/Exe";
+            Console.WriteLine("watcher started!");
+            watcher.Run();
+            var cmd = "";
+            do{
+                cmd = System.Console.ReadLine();
+
+            }while(!checkCommand(cmd));
+            
+            watcher.FileWatcher.Dispose();
+        }
+
+        static bool checkCommand(string cmd){
+            if(cmd.ToLower().Trim()!="stop"){
+                Console.WriteLine("Invalid command! type stop to terminate");
+                return false;
+            }
+            return true;
+
+        }
+    }
+}
---
+++ Monitor/FileWatcher.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace filemon.Monitor
+{
+    public class Watcher
+    {
+        public string Path { get; set; }
+        public FileSystemWatcher FileWatcher { get; set; }
+
+        private NotifyFilters filters = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName 
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size;
+        public NotifyFilters Filters { 
+            get {return filters;} 
+            set {filters = value;}    
+        }
+        public void  Run(){
+ 
+
+            FileWatcher = new FileSystemWatcher(Path);
+
+            FileWatcher.NotifyFilter = this.Filters;
+
+            FileWatcher.Changed += OnChanged;
+            FileWatcher.Created += OnCreated;
+            FileWatcher.Deleted += OnDeleted;
+            FileWatcher.Renamed += OnRenamed;
+            FileWatcher.Error   += OnError;
+
+            FileWatcher.IncludeSubdirectories = true;
+            FileWatcher.EnableRaisingEvents = true; 
+        }
+
+
+        private void OnChanged(object sender, FileSystemEventArgs e){
+            Console.WriteLine("File "+e.ChangeType.ToString()+". Name: "+e.Name);
+        }
+        private void OnCreated(object sender, FileSystemEventArgs e){
+            Console.WriteLine("File "+e.ChangeType.ToString()+". Name: "+e.Name);
+
+        }
+        private void OnDeleted(object sender, FileSystemEventArgs e){
+            Console.WriteLine("File "+e.ChangeType.ToString()+". Name: "+e.Name);
+
+        }
+        private void OnRenamed(object sender, RenamedEventArgs e){
+            Console.WriteLine("File "+e.ChangeType.ToString()+". Name: "+e.Name);
+
+        }
+        private void OnError(object sender,  ErrorEventArgs e){
+            Console.WriteLine("File Error! "+e.GetException().Message);
+
+        }
+    }
+}
