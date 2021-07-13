dc520cf49f9ca9735ffc560410575baa200799c4
bahram <bahram.ghahari@gmail.com>
2021-07-13 00:14:55 -0400

    web hook handler

---
+++ Monitor/Handlers/WebhookHandler.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+
+namespace filemon.Monitor{
+    public class WebhookHandler : IChangeHandler
+    { 
+        public void OnChanged(object sender, FileSystemEventArgs e){
+             
+        }
+        public void OnCreated(object sender, FileSystemEventArgs e){ 
+
+        }
+        public void OnDeleted(object sender, FileSystemEventArgs e){ 
+
+        }
+        public void OnRenamed(object sender, RenamedEventArgs e){ 
+
+        }
+        public void OnError(object sender,  ErrorEventArgs e){ 
+
+        }
+    }
+}
